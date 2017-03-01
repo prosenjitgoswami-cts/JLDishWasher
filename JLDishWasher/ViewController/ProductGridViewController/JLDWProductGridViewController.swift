ab002d79ea4ca6877fb02d859b1b1dc64ee00079
hello <prosenjit.goswami@gosnizant>
2017-03-01 19:47:22 +0530

    Implement Tap functionality on product

--- JLDishWasher/ViewController/ProductGridViewController/JLDWProductGridViewController.swift
+++ JLDishWasher/ViewController/ProductGridViewController/JLDWProductGridViewController.swift
@@ -8,121 +8,164 @@
 
 import UIKit
 
+
+//MARK:------------------------------- * ----------------------------------
+//MARK: ViewController Life Cycle
 class JLDWProductGridViewController: UIViewController {
-    @IBOutlet weak var productGridCollectionView: UICollectionView!
+	@IBOutlet weak var productGridCollectionView: UICollectionView!
 
-    var cellsPerRow:CGFloat = 4
-    let cellPadding:CGFloat = 1
-    var productDatasource: [Product]?
-    lazy var presenter: JLDWProductGridViewControllerPresenter = {
-        return JLDWProductGridViewControllerPresenter()
-    }()
+	var cellsPerRow:CGFloat = 4
+	let cellPadding:CGFloat = 1
+	var productDatasource: [Product]?
+	lazy var presenter: JLDWProductGridViewControllerPresenter = {
+		return JLDWProductGridViewControllerPresenter()
+	}()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.productGridCollectionView.delegate = self
-        self.productGridCollectionView.dataSource = self
-        productDatasource = [Product]()
-        configureUI()
-        fetchService ()
-        
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.productGridCollectionView.delegate = self
+		self.productGridCollectionView.dataSource = self
+		productDatasource = [Product]()
+		configureUI()
+		fetchService ()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	}
+
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+		let destinationVC: JLDWProductDetailsViewController = segue.destination as! JLDWProductDetailsViewController
+
+		let selectedProduct: Product = sender as! Product
+		destinationVC.setProduct(withProduct: selectedProduct)
 
 
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-
+	}
 }
+
+//MARK:------------------------------- * ----------------------------------
 //MARK: Private Method
 
 extension JLDWProductGridViewController {
 
-    internal func fetchService() {
+	/**
+	Invoke Web Service and Get Products details and Refresh UI
+	@param failed: Fail Block
 
-        self.presenter.fetchProductList(failed: { (error) in
-            print(error)
-        }) { (products) in
+	*/
+	internal func fetchService() {
 
-            self.setNavigationBarTitleWith(productCount: (products?.count)!)
-            self.productDatasource = products
-            self.productGridCollectionView.reloadData()
-        }
-    }
+		self.presenter.fetchProductList(failed: { (error) in
+			print(error)
+		}) { (products) in
 
-    internal func configureUI() {
+			self.updateUI(withProducts: products)
+		}
+	}
 
-        setNavigationBarTitleWith(productCount: 0)
-    }
 
-    private func setNavigationBarTitleWith(productCount: Int) {
+	/**
+	Update UI
+	@param products: Collection of Product
+	*/
+	internal func updateUI(withProducts products: [Product]?) {
 
-        var pageTitle: String = PageTitleProductGridVC;
+		if let products = products {
+			setNavigationBarTitle(withProductCount:products.count)
 
-        if (productCount > 0) {
+		}
+		productDatasource = products
+		productGridCollectionView.reloadData()
+	}
 
-            pageTitle.append("("+"\(productCount)"+")")
-        }
-        self.title = pageTitle
+	/**
+	Navigate to JLDWProductDetailsViewController though Segue
+	@param product: Selected product details
+	*/
+	internal func navigate(withSelectedProduct product: Product?) {
 
-    }
+		if let product: Product = product {
+			self.performSegue(withIdentifier: kSegurID_ToJLDWProductDetailsViewController, sender: product)
+		}
+	}
+
+	/**
+	Set Navigation Bar Title
+	*/
+	internal func configureUI() {
+
+		setNavigationBarTitle(withProductCount: 0)
+	}
+
+
+	/**
+	Navigate to JLDWProductDetailsViewController though Segue
+	@param product: Selected product details
+	*/
+	private func setNavigationBarTitle(withProductCount productCount: Int) {
+
+		var pageTitle: String = PageTitleProductGridVC;
+
+		if (productCount > 0) {
+
+			pageTitle.append("("+"\(productCount)"+")")
+		}
+		self.title = pageTitle
+
+	}
 }
+
+
+//MARK:------------------------------- * ----------------------------------
+//MARK: CollectionView DataSource and Delegate
 
 extension JLDWProductGridViewController: UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
 
 
+	// Set Number of Items in section
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
-        return (productDatasource?.count)!
-    }
-
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-        let cellReuseIdentifier: String = JLDWProductGridVCCollectionViewCellID
-
-        let cell: JLDWProductGridVCCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! JLDWProductGridVCCollectionViewCell
-        cell.backgroundColor = UIColor.green;
-
-        if let product = productDatasource?[indexPath.row] {
-            cell.setProductDetails(product: product)
-        }
-
-        return cell
-    }
+		return (productDatasource?.count)!
+	}
 
 
-    /*   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+	// Create CollectionViewCell
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-     let width = CGFloat(Double(self.view.frame.size.width) - leftAndRightPaddings)/4
+		let cellReuseIdentifier: String = kCellID_JLDWProductGridVCCollectionViewCell
 
-     return CGSize(width: 100, height: 100)
-     }
-     */
+		let cell: JLDWProductGridVCCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! JLDWProductGridVCCollectionViewCell
+		cell.backgroundColor = UIColor.init(colorLiteralRed: <#T##Float#>, green: <#T##Float#>, blue: <#T##Float#>, alpha: <#T##Float#>);
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right:10)
-    }
+		if let product = productDatasource?[indexPath.row] {
+			cell.setProductDetails(product: product)
+		}
+
+		return cell
+	}
+
+	// Set Size in CollectionView cell
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return UIEdgeInsets(top: 0, left: 10, bottom: 0, right:10)
+	}
 
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let widthMinusPadding = UIScreen.main.bounds.width - (cellPadding * cellsPerRow+1)
-        let width = widthMinusPadding / cellsPerRow
-        return CGSize(width: 300.0, height: max(width, 300.0))
-    }
+	// Set Size in collectionView
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let widthMinusPadding = UIScreen.main.bounds.width - (cellPadding * cellsPerRow+1)
+		let width = widthMinusPadding / cellsPerRow
+		return CGSize(width: width, height: max(width, 300.0))
+	}
 
+
+	// Select Product Item
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+		let product = productDatasource?[indexPath.row]
+		navigate(withSelectedProduct: product)
+		
+	}
 }
+
 
 

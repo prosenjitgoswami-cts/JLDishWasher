4a6e20525f26d034e0eb94b6aafd069cd4e378d1
hello <prosenjit.goswami@gosnizant>
2017-03-01 21:35:05 +0530

    Create JLDWProductDetailsVCTests file and add test target reference

--- JLDishWasher/Model/SpecificProductInfo/SpecificProductInfo.swift
+++ JLDishWasher/Model/SpecificProductInfo/SpecificProductInfo.swift
@@ -11,6 +11,35 @@ class SpecificProductInfo: Product {
 
 	var media: Media?
 	var details: Details?
-	
+	var displaySpecialOffer :String?
 
+
+}
+
+//MARK:------------------------------- * ----------------------------------
+//MARK: Public Method
+extension SpecificProductInfo {
+
+	public func initWith(response: Dictionary<String, Any>?) {
+
+		guard let response = response else{
+			return;
+		}
+		super.initWithResponse(response: response)
+
+		if let displaySpecialOfferStr = response[kDictDisplaySpecialOffer] as! String? {
+
+			displaySpecialOffer = displaySpecialOfferStr;
+		}
+	}
+}
+
+//MARK:------------------------------- * ----------------------------------
+//MARK: Private Method
+extension SpecificProductInfo {
+
+private func bindMedia(with media:Dictionary<String, Any>?) {
+
+
+	}
 }

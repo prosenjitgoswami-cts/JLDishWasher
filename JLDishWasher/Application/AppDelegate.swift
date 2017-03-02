35ec758519ba43f1d5f12908334b9d44402688c6
hello <hellomac@macpcaim00160.local>
2017-03-02 16:17:50 +0530

    Create folder Structure for application

--- JLDishWasher/Application/AppDelegate.swift
+++ JLDishWasher/Application/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  JLDishWasher
+//  DishWasher
 //
-//  Created by Prsenjit Goswami on 02/03/17.
+//  Created by Prsenjit Goswami on 01/03/17.
 //  Copyright © 2017 CTS. All rights reserved.
 //
 
